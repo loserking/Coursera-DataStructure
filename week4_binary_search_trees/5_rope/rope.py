f21bb3492dc52ffe294c37d4842b1fda321285c8
Yueh-Zheng Lin <b99501032@gmail.com>
2019-08-14 16:08:45 +0800

    rope

--- week4_binary_search_trees/5_rope/rope.py
+++ week4_binary_search_trees/5_rope/rope.py
@@ -8,9 +8,12 @@ class Rope:
 	def result(self):
 		return self.s
 	def process(self, i, j, k):
-                # Write your code here
-                
-
+            substring = self.s[i:j + 1]
+            self.s = self.s[:i] + self.s[j + 1:]
+            if k == 0:
+                self.s = substring + self.s
+            else:
+                self.s = self.s[:k] + substring + self.s[k:]
 rope = Rope(sys.stdin.readline().strip())
 q = int(sys.stdin.readline())
 for _ in range(q):
